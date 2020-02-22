085d6ea439b51a798f7f2a0b4d5558d0489667ed
Nakul Pitolwala <nakul.pitolwala@gmail.com>
2020-02-22 23:21:49 +0530

    Added bootstrap and jobs page

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+import os
+cwd = os.getcwd()
+base_dir = os.path.dirname(cwd)
+
+def home(request):
+    jobs = Job.objects
+    im = '\media\images\sr40.jpg'
+    return render(request, 'jobs/home.html', {'jobs': jobs, 'image': im})
